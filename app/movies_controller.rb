f59904e06a4d4fc81f9000d337c8fec5e3200fc8
Chance Smith <chancesmith97@hotmail.com>
2019-08-04 22:02:42 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -94,7 +94,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   tbd = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  Movie.
+  Movie.destroy(tbd)
 end
 
 def can_destroy_all_items_at_once
