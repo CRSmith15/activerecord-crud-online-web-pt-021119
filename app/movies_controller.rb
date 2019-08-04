7b2a7ffbc8deb901ae702613a338af5cc87ae561
Chance Smith <chancesmith97@hotmail.com>
2019-08-04 21:49:50 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -40,7 +40,8 @@ def can_get_the_first_item_in_the_database
 end
 
 def can_get_the_last_item_in_the_database
-  Movie.last.title
+  movie = Movie.last
+  movie
 end
 
 def can_get_size_of_the_database
@@ -48,7 +49,8 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.find_by(id: 1).title
+ movie = Movie.find_by(id: 1)
+ movie
 end
 
 def can_find_by_multiple_attributes
