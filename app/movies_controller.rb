08398eb8dd517060a611adb184ac9fd21c3e68b6
Chance Smith <chancesmith97@hotmail.com>
2019-08-04 21:58:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -80,8 +80,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   movie = Movie.create(title: "Wat?")
-  
-  __
+  movie.update(title: "Wat, huh?")
 end
 
 def can_update_multiple_items_at_once
